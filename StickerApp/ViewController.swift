5f3890e79ccce4706423566e3d77439cef977cfb
Tran Cam <camtv.frmobile@gmail.com>
2022-11-05 16:33:00 +0700

    upload code

---
+++ StickerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StickerApp
+//
+//  Created by Trần Văn Cam on 05/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
