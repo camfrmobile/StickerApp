6bc686e7ee92150a56302481a025581de8218864
Tran Cam <camtv.frmobile@gmail.com>
2022-11-07 21:16:25 +0700

    edit width button

--- StickerApp/Intro/IntroViewController.swift
+++ StickerApp/Intro/IntroViewController.swift
@@ -12,8 +12,9 @@ class IntroViewController: UIViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var introImageView: UIImageView!
-    
+    @IBOutlet weak var view4: UIView!
     @IBOutlet weak var widthButtonConstraint: NSLayoutConstraint!
+    
     var index = 1
     
     override func viewDidLoad() {
@@ -46,21 +47,21 @@ class IntroViewController: UIViewController {
         view.backgroundColor = #colorLiteral(red: 0.4186902642, green: 0.09940036386, blue: 0.9996766448, alpha: 1)
         introImageView.image = UIImage(named: "intro_1")
         titleLabel.text = "Fun Stickers, 50.000+ Stickers"
-        //nextButton.widthAnchor.constraint(equalToConstant: 104).isActive = true
-        widthButtonConstraint.constant = 104
+        
+        widthButtonConstraint.constant = view4.bounds.width * 0.3
     }
     func setupIntro2() {
         view.backgroundColor = #colorLiteral(red: 0.9889518619, green: 0.8661288619, blue: 0.068666704, alpha: 1)
         introImageView.image = UIImage(named: "intro_2")
         titleLabel.text = "Create your favourite stickers"
-        //nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2).isActive = true
-        widthButtonConstraint.constant = 207
+        
+        widthButtonConstraint.constant = view4.bounds.width * 2/3
     }
     func setupIntro3() {
         view.backgroundColor = #colorLiteral(red: 0.9996364713, green: 0.2497695088, blue: 0.6706078053, alpha: 1)
         introImageView.image = UIImage(named: "intro_3")
         titleLabel.text = "prank your friends in iMessags"
         //nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.75).isActive = true
-        widthButtonConstraint.constant = 311
+        widthButtonConstraint.constant = view4.bounds.width
     }
 }
